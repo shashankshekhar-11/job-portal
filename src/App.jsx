9f70984adbef8604acc541f80e69463ff9bba89f
Shashank <shashankshekhar746@gmail.com>
2024-10-13 20:10:20 +0530

    Routes created

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,54 @@
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './App.css'
-import { Button } from './components/ui/button'
+import AppLayout from '@/layouts/AppLayout'
+import LandingPage from '@/pages/LandingPage'
+import Onboarding from '@/pages/Onboarding'
+import JobListing from './pages/JobListing'
+import JobPage from './pages/JobPage'
+import PostJob from './pages/PostJob'
+import SavedJobs from './pages/SavedJobs'
+import MyJobs from './pages/MyJobs'
 
+const router = createBrowserRouter([
+  {
+    element :<AppLayout />,
+    children:[
+      {
+        path :'/',
+        element :<LandingPage />
+      },
+      {
+        path :'/onboarding',
+        element :<Onboarding />
+    
+      },
+      {
+        path : '/jobs',
+        element:<JobListing />
+      },
+      {
+        path: '/job/:id',
+        element:<JobPage />
+      },
+      {
+        path: '/post-job',
+        element: <PostJob />
+      },
+      {
+        path: '/saved-job',
+        element: <SavedJobs />
+      },
+      {
+        path: '/my-jobs',
+        element: <MyJobs />
+      }
+    ]
+  }
+])
 function App() {
-
-
   return (
-    <div>
-    <h1>Hello</h1>
-      <Button>login</Button>
-    </div>
-   
+   <RouterProvider router={router} />
   )
 }
 
